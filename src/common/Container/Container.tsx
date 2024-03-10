6e03b3b675d5b579e908ed8e8e7f3183b4a1622b
freekos <gazizbolat2@gmail.com>
2024-03-10 19:47:54 +0500

    feat: add container

---
+++ src/common/Container/Container.tsx
@@ -0,0 +1,23 @@
+import { ComponentProps, ReactNode, forwardRef } from 'react';
+import styles from './styles.module.scss';
+
+interface ContainerProps extends ComponentProps<'div'> {
+	left?: ReactNode;
+	right?: ReactNode;
+}
+
+export const Container = forwardRef<HTMLDivElement, ContainerProps>(
+	({ left, right, children, ...props }: ContainerProps, ref) => {
+		return (
+			<div className={styles.container} ref={ref} {...props}>
+				{(left || right) && (
+					<aside className={styles.container__action}>
+						<div>{left}</div>
+						<div>{right}</div>
+					</aside>
+				)}
+				{children}
+			</div>
+		);
+	}
+);
