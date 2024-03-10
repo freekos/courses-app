d815629d145b1a9fcab43f4b78c791b19de4bf82
freekos <gazizbolat2@gmail.com>
2024-03-11 04:17:31 +0500

    chore: add courses to constants

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+import { Course } from './types/course';
+
+export const COURSES: Course[] = [
+	{
+		id: 1,
+		title: 'Angular',
+		description: 'Lorem',
+		authors: ['Golat Gaziz'],
+		duration: '1h',
+		createdAt: '23.03.2023',
+	},
+	{
+		id: 2,
+		title: 'React',
+		description: 'Lorem',
+		authors: ['Golat Gaziz'],
+		duration: '1h',
+		createdAt: '23.03.2023',
+	},
+];
