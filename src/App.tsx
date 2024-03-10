5c3e0223d283a947408905350e2c29fbbab48aa2
freekos <gazizbolat2@gmail.com>
2024-03-11 04:41:35 +0500

    feat: add app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Courses } from './components/Courses';
+import { Header } from './components/Header';
+import { COURSES } from './constants';
+
 function App() {
-  return <div>React</div>;
+	return (
+		<div className='app'>
+			<Header />
+			<main className='main'>
+				<Courses courses={COURSES} />
+			</main>
+		</div>
+	);
 }
 
 export default App;
