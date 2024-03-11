096587c455e944e2f75a0dcb2b64a2d647a0960f
freekos <gazizbolat2@gmail.com>
2024-03-11 05:25:21 +0500

    chore: add courses map with authors-fullname

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import { Courses } from './components/Courses';
 import { Header } from './components/Header';
-import { COURSES } from './constants';
+import { AUTHORS_LIST, COURSES } from './constants';
 
 function App() {
+	const courses = COURSES.map((course) => {
+		const authors = course.authors.map((author) => {
+			const findAuthor = AUTHORS_LIST.find((item) => item.id === author);
+			return findAuthor.name;
+		});
+		return {
+			...course,
+			authors,
+		};
+	});
+
 	return (
 		<div className='app'>
 			<Header />
 			<main className='main'>
-				<Courses courses={COURSES} />
+				<Courses courses={courses} />
 			</main>
 		</div>
 	);
