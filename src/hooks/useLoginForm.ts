e58c52b75ff3f70f073f3cec726fdd59ebd5014a
freekos <gazizbolat2@gmail.com>
2024-03-18 18:19:05 +0500

    feat: add login-form-hook

---
+++ src/hooks/useLoginForm.ts
@@ -0,0 +1,34 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const loginSchema = z.object({
+	email: z
+		.string({ required_error: 'Email is required' })
+		.email({ message: 'Invalid email format' }),
+	password: z
+		.string({ required_error: 'Password is required' })
+		.min(8, { message: 'Min password length is 8' }),
+});
+export type LoginSchema = z.infer<typeof loginSchema>;
+
+export const useLoginForm = () => {
+	const form = useForm<LoginSchema>({
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+		resolver: zodResolver(loginSchema),
+	});
+
+	const handleLogin = async (data: LoginSchema) => {
+		try {
+			const res = await fetch('');
+		} catch (err) {
+			console.log(err);
+			form.setError('root', { message: err });
+		}
+	};
+
+	return { form, handleLogin };
+};
