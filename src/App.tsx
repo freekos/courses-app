c8dfd55cfa23e41e43438ac710f6cc4e84cf8a59
freekos <gazizbolat2@gmail.com>
2024-03-18 18:19:39 +0500

    feat: change hard-code to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,16 @@
-import { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import { Courses } from './components/Courses';
 import { Header } from './components/Header';
-import { CourseInfo } from './components/CourseInfo';
-import { Course } from './types/course';
-import { AUTHORS_LIST, COURSES } from './constants';
-import { Button } from './common/Button';
+import { Routing } from './routing';
 
 function App() {
-	const [selectedCourse, setSelectedCourse] = useState<Course | null>(null);
-
-	const courses = COURSES.map((course) => {
-		const authors = course.authors.map((author) => {
-			const findAuthor = AUTHORS_LIST.find((item) => item.id === author);
-			return findAuthor.name;
-		});
-		return {
-			...course,
-			authors,
-		};
-	});
-
 	return (
 		<div className='app'>
 			<Header />
 			<main className='main'>
-				{selectedCourse ? (
-					<CourseInfo
-						course={selectedCourse}
-						actions={
-							<Button onClick={() => setSelectedCourse(null)}>Back</Button>
-						}
-					/>
-				) : (
-					<Courses
-						onShow={(course: Course) => setSelectedCourse(course)}
-						courses={courses}
-					/>
-				)}
+				<BrowserRouter>
+					<Routing />
+				</BrowserRouter>
 			</main>
 		</div>
 	);
