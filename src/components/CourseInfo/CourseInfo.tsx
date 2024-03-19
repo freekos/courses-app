89def34bf7c49970b70529c81ec4b04028f83856
freekos <gazizbolat2@gmail.com>
2024-03-19 08:00:19 +0500

    refactor: add react-router feats

--- src/components/CourseInfo/CourseInfo.tsx
+++ src/components/CourseInfo/CourseInfo.tsx
@@ -1,24 +1,26 @@
-import { ReactNode } from 'react';
-
-import { Course } from 'src/types/course';
 import { Container } from 'src/common/Container';
 import { CourseInfoCard } from './components/CourseInfoCard';
 import styles from './styles.module.scss';
+import { Button } from 'src/common/Button';
+import { useNavigate, useParams } from 'react-router-dom';
+import { COURSES } from 'src/constants';
 
-interface CourseInfoProps {
-	course: Course;
-	actions?: ReactNode;
-}
+export const CourseInfo = () => {
+	const { id } = useParams();
+	const navigate = useNavigate();
 
-export const CourseInfo = ({ course, actions }: CourseInfoProps) => {
+	const course = COURSES.find((item) => item.id === id);
+
 	return (
 		<Container isDark>
 			<div className={styles.course_info}>
-				<h5 className={styles.course_info__title}>{course.title}</h5>
+				<h5 className={styles.course_info__title}>{course!.title}</h5>
 				<div className={styles.course_info__card}>
-					<CourseInfoCard course={course} />
+					<CourseInfoCard course={course!} />
 				</div>
-				<div className={styles.course_info__actions}>{actions}</div>
+				<div className={styles.course_info__actions}>
+					<Button onClick={() => navigate(-1)}>Back</Button>
+				</div>
 			</div>
 		</Container>
 	);
