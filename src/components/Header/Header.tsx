95e4dfb58c145eae307dc2118329b9fb3d428840
freekos <gazizbolat2@gmail.com>
2024-03-19 07:50:03 +0500

    feat: change header-view and add login-button-show logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { Link, useLocation } from 'react-router-dom';
+
 import { SessionContainer } from 'src/containers/SessionContainer';
 import { Button } from 'src/common/Button';
 import { HeaderWrapper } from './components/HeaderWrapper';
@@ -6,6 +8,8 @@ import { Logo } from './components/Logo';
 import styles from './styles.module.scss';
 
 export const Header = () => {
+	const location = useLocation();
+
 	return (
 		<header className={styles.header}>
 			<HeaderWrapper
@@ -17,10 +21,10 @@ export const Header = () => {
 				right={
 					<div className={styles.header__right}>
 						<SessionContainer
-							render={({ isAuth, onLogin, onLogout }) =>
-								isAuth ? (
+							render={({ session, onLogout }) =>
+								session ? (
 									<>
-										<Username name='Harry Poter' />
+										<Username name={session.user.name} />
 										<Button
 											style={{ textTransform: 'uppercase' }}
 											onClick={onLogout}
@@ -28,16 +32,14 @@ export const Header = () => {
 											Logout
 										</Button>
 									</>
-								) : (
-									<>
-										<Button
-											style={{ textTransform: 'uppercase' }}
-											onClick={onLogin}
-										>
+								) : location.pathname !== '/login' &&
+								  location.pathname !== '/registration' ? (
+									<Link to='/login'>
+										<Button style={{ textTransform: 'uppercase' }}>
 											Login
 										</Button>
-									</>
-								)
+									</Link>
+								) : null
 							}
 						/>
 					</div>
