a2f1d3820feb0d1cd36910145d84819e99a74c95
freekos <gazizbolat2@gmail.com>
2024-03-19 14:25:06 +0500

    fix: change set-minutes to utc format

--- src/helpers/getDurationTime.ts
+++ src/helpers/getDurationTime.ts
@@ -1,7 +1,7 @@
 export const getDurationTime = (duration: number) => {
-	const date = new Date();
-	date.setMinutes(duration);
-	const hours = date.getHours();
-	const minutes = date.getMinutes();
+	const date = new Date(0);
+	date.setUTCMinutes(duration);
+	const hours = date.getUTCHours();
+	const minutes = date.getUTCMinutes();
 	return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}`;
 };
