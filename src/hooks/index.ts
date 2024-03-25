0dcd9203bf1a5b7b83f0647e8a8c8b54f8353a46
freekos <gazizbolat2@gmail.com>
2024-03-25 12:27:33 +0500

    chore: add re-export for hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,7 @@
+export * from './useAuthorForm';
+export * from './useCreateCourseForm';
+export * from './useLoginForm';
+export * from './useRegistrationForm';
+export * from './useSession';
+export * from './useAuthors';
+export * from './useCourses';
