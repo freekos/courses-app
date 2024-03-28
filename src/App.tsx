36b4885c3344e5d1c7946f760e6ae33e3e692838
freekos <gazizbolat2@gmail.com>
2024-03-28 13:29:15 +0500

    feat: add redux-persist logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,24 @@ import { Provider } from 'react-redux';
 import { Header } from './components/Header';
 import { Routing } from './routing';
 import { store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+export const persistor = persistStore(store);
 
 function App() {
 	return (
 		<Provider store={store}>
-			<BrowserRouter>
-				<div className='app'>
-					<Header />
-					<main className='main'>
-						<Routing />
-					</main>
-				</div>
-			</BrowserRouter>
+			<PersistGate loading={null} persistor={persistor}>
+				<BrowserRouter>
+					<div className='app'>
+						<Header />
+						<main className='main'>
+							<Routing />
+						</main>
+					</div>
+				</BrowserRouter>
+			</PersistGate>
 		</Provider>
 	);
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './rootReducer';
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+const persistConfig = {
+	version: 1,
+	key: 'root',
+	storage,
+	whitelist: ['user'],
+};
+
+const persistedReducer = persistReducer<ReturnType<typeof rootReducer>>(
+	persistConfig,
+	rootReducer
+);
+
 export const store = configureStore({
-	reducer: rootReducer,
+	reducer: persistedReducer,
+	devTools: process.env.NODE_ENV !== 'production',
 });
