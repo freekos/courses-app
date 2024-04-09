8e9ab409415042df333ec3ee70afe909cad69cc3
freekos <gazizbolat2@gmail.com>
2024-04-09 17:18:55 +0500

    test: rewrite course-card tests

--- src/components/Courses/components/CourseCard/tests/CourseCard.test.tsx
+++ src/components/Courses/components/CourseCard/tests/CourseCard.test.tsx
@@ -1,42 +1,48 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { CourseCard } from '../CourseCard';
 
-const mockCourse = {
-	id: '1',
-	title: 'Test Course',
-	description: 'This is a test course description.',
-	authors: ['John Doe', 'Jane Smith'],
-	duration: 120,
-	creationDate: '2024-04-01',
-};
-
 describe('CourseCard', () => {
+	const getComponent = () => {
+		const mockCourse = {
+			id: '1',
+			title: 'Test Course',
+			description: 'This is a test course description.',
+			authors: ['John Doe', 'Jane Smith'],
+			duration: 120,
+			creationDate: '2024-04-01',
+		};
+
+		return <CourseCard course={mockCourse} />;
+	};
+
 	it('should display title', () => {
-		const { getByTestId } = render(<CourseCard course={mockCourse} />);
-		expect(getByTestId('title')).toHaveTextContent('Test Course');
+		render(getComponent());
+		expect(screen.getByText('Test Course')).toBeInTheDocument();
 	});
 
 	it('should display description', () => {
-		const { getByText } = render(<CourseCard course={mockCourse} />);
-		expect(getByText('This is a test course description.')).toBeInTheDocument();
+		render(getComponent());
+		expect(
+			screen.getByText('This is a test course description.')
+		).toBeInTheDocument();
 	});
 
 	it('should display duration in the correct format', () => {
-		const { getByText } = render(<CourseCard course={mockCourse} />);
-		expect(getByText(/Duration:/)).toBeInTheDocument();
-		expect(getByText('02:00')).toBeInTheDocument();
+		render(getComponent());
+		expect(screen.getByText('Duration:')).toBeInTheDocument();
+		expect(screen.getByText('02:00')).toBeInTheDocument();
 	});
 
 	it('should display authors list', () => {
-		const { getByText } = render(<CourseCard course={mockCourse} />);
-		expect(getByText(/Authors:/)).toBeInTheDocument();
-		expect(getByText('John Doe, Jane Smith')).toBeInTheDocument();
+		render(getComponent());
+		expect(screen.getByText('Authors:')).toBeInTheDocument();
+		expect(screen.getByText('John Doe, Jane Smith')).toBeInTheDocument();
 	});
 
 	it('should display created date in the correct format', () => {
-		const { getByText } = render(<CourseCard course={mockCourse} />);
-		expect(getByText(/Created:/)).toBeInTheDocument();
-		expect(getByText('2024-04-01')).toBeInTheDocument();
+		render(getComponent());
+		expect(screen.getByText('Created:')).toBeInTheDocument();
+		expect(screen.getByText('2024-04-01')).toBeInTheDocument();
 	});
 });
